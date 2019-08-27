7ba228f68f211ec7ca17c00c0ec668700f6e18ca
Jakub Kiczko <jakub@kiczko.net>
2019-08-27 10:01:18 +0200

    GeoIP.

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var ip2locv4 = require('ip2location-nodejs');
+var ip2locv6 = require('ip2location-nodejs');
+
+ip2locv4.IP2Location_init("db/ipv4.BIN");
+ip2locv6.IP2Location_init("db/ipv6.BIN");
+
+router.get('/check/:checkIp', function(req, res, next) {
+    var result = {};
+
+    if (/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(req.params.checkIp)) {
+        result = ip2locv4.IP2Location_get_all(req.params.checkIp);
+        console.log("Checking IPv4: " + req.params.checkIp);
+    } else {
+        result = ip2locv6.IP2Location_get_all(req.params.checkIp);
+        console.log("Checking IPv6: " + req.params.checkIp);
+    }
+
+    res.send(result);
+});
+
+module.exports = router;
