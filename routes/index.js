41870b36ca89827b161509f98ac762cfa5fc6b1e
Jakub Kiczko <jakub@kiczko.net>
2019-08-30 14:31:00 +0200

    Zwracanie samego country.

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,14 @@ router.get('/check/:checkIp', function(req, res, next) {
     var result = {};
 
     if (/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(req.params.checkIp)) {
-        result = ip2locv4.IP2Location_get_all(req.params.checkIp);
+        result = ip2locv4.IP2Location_get_country_short(req.params.checkIp);
         console.log("Checking IPv4: " + req.params.checkIp);
     } else {
-        result = ip2locv6.IP2Location_get_all(req.params.checkIp);
+        result = ip2locv6.IP2Location_get_country_short(req.params.checkIp);
         console.log("Checking IPv6: " + req.params.checkIp);
     }
 
-    res.send(result);
+    res.send({country: result});
 });
 
 module.exports = router;
