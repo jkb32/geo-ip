ca8cbf99696a38992de565bd9c38e79a45649d56
Jakub Kiczko <jakub@kiczko.net>
2020-04-03 16:18:25 +0200

    Baza.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var ip2locv6 = require('ip2location-nodejs');
+ip2locv6.IP2Location_init("db/ipv6.BIN");
+
 router.get('/check/:checkIp', function(req, res, next) {
     var result = {};
 
     if (/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(req.params.checkIp)) {
-        var ip2locv4 = require('ip2location-nodejs');
-        ip2locv4.IP2Location_init("db/ipv4.BIN");
-        result = ip2locv4.IP2Location_get_country_short(req.params.checkIp);
+        result = ip2locv6.IP2Location_get_country_short(req.params.checkIp);
         console.log("Checking IPv4: " + req.params.checkIp);
     } else {
-        var ip2locv6 = require('ip2location-nodejs');
-        ip2locv6.IP2Location_init("db/ipv6.BIN");
         result = ip2locv6.IP2Location_get_country_short(req.params.checkIp);
         console.log("Checking IPv6: " + req.params.checkIp);
     }
